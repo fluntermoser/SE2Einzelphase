637d210db84a4be77d464038e7339625c8998720
Florian Untermoser <fluntermoser@edu.aau.at>
2018-03-05 20:53:01 +0100

    finished feature - added layout + logic for palindrome check

--- app/src/main/java/aau/edu/fluntermoser/se2einzelphase/MainActivity.java
+++ app/src/main/java/aau/edu/fluntermoser/se2einzelphase/MainActivity.java
@@ -1,7 +1,12 @@
 package aau.edu.fluntermoser.se2einzelphase;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkPalindrom(View view) {
+        EditText editText = (EditText) findViewById(R.id.palindromeText);
+        String text = editText.getText().toString();
+
+        if(text.isEmpty() || text.length() == 0 || text == null) {
+            writeMessage(R.string.errorEmptyString);
+        } else if (text.length() < 5) {
+            writeMessage(R.string.errorStringToShort);
+        }
+        else if(isPalindrome(text)) {
+            writeMessage(R.string.stringsIsPalindrome);
+        } else {
+            writeMessage(R.string.stringIsNotPalindrome);
+        }
+
+    }
+
+    public void writeMessage(int resourceId) {
+        String message = getResources().getText(resourceId).toString();
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast.makeText(context, message, duration).show();
+        TextView resultView = findViewById(R.id.resultText);
+        resultView.setText(message);
+    }
+
+    public static boolean isPalindrome(String str)	{
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
 }
